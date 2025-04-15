dac5b6618cb981f9755d74e0f87543d6e42b7629
sobanomar <sobanumar0@gmail.com>
2025-04-16 04:27:28 +0500

    fix: fix

--- src/layout/AppLayout.tsx
+++ src/layout/AppLayout.tsx
@@ -3,7 +3,7 @@ import { Outlet } from "react-router";
 import AppHeader from "./AppHeader";
 import Backdrop from "./Backdrop";
 import AppSidebar from "./AppSidebar";
-import CountdownTimer from "../components/common/CountDownTimer";
+import CountdownTimer from "../components/common/CountdownTimer";
 
 const LayoutContent: React.FC = () => {
   const { isExpanded, isHovered, isMobileOpen } = useSidebar();
