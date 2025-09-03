26c86fedc922c106c639bc71ecd66339b21262ba
sobanomar <sobanumar0@gmail.com>
2025-09-04 03:30:09 +0500

    feat: page meta added

--- src/pages/AuthPages/ForgotPassword.tsx
+++ src/pages/AuthPages/ForgotPassword.tsx
@@ -6,8 +6,8 @@ export default function ForgotPassword() {
   return (
     <>
       <PageMeta
-        title="React.js SignIn Dashboard | TailAdmin - Next.js Admin Dashboard Template"
-        description="This is React.js SignIn Tables Dashboard page for TailAdmin - React.js Tailwind CSS Admin Dashboard Template"
+        title="Forgot Password"
+        description="Forgot password page peakprofit dasboard"
       />
       <AuthLayout>
         <ForgotPasswordForm />
