7b40f8ad1db08ca151e3ad43b1543fb546255eba
sobanomar <sobanumar0@gmail.com>
2025-09-08 20:53:41 +0500

    feat: referral code integration in sign up

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -79,7 +79,7 @@ define(['./workbox-e755d862'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.4e0r3j6lelg"
+    "revision": "0.3ethuujruqg"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
--- src/api/axiosInstance.ts
+++ src/api/axiosInstance.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const baseUrl = import.meta.env.VITE_API_BASE_URL_LIVE;
+const baseUrl = import.meta.env.VITE_API_BASE_URL;
 
 const axiosInstance = axios.create({
   baseURL: baseUrl,
